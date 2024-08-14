8b8bfbd4422b3ee55396fc5a854cd70c0479a66d
Marvin <okongomarvin971@gmail.com>
2024-08-14 16:09:59 +0300

    download

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,39 @@
-import { Button } from "@/components/ui/button";
-import { FiDownload } from "react-icons/fi"
+"use client";
 
-//components
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+// components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
 
+const PNG_FILE_URL = '/assets/cv.pdf';
+
 const Home = () => {
+  const downloadFile = async (url) => {
+    try {
+      const response = await fetch(url);
+      
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+      
+      const blob = await response.blob();
+      const blobURL = window.URL.createObjectURL(blob);
+      const fileName = url.split('/').pop();
+      const aTag = document.createElement("a");
+      aTag.href = blobURL;
+      aTag.setAttribute("download", fileName);
+      document.body.appendChild(aTag);
+      aTag.click();
+      aTag.remove();
+    } catch (error) {
+      console.error('Failed to download file:', error);
+      // Optionally, display an error message to the user
+    }
+  };
+
   return (
     <section className="h-full">
       <link rel="icon" href="./logo.svg" />
@@ -17,32 +44,35 @@ const Home = () => {
             <span className="text-xl">Full-stack Developer</span>
             <h1 className="h1">
               Hello I'm <br /> <span>Marvin <span className="text-accent"> Okongo</span></span>
-              </h1>
-              <p className="max-w-[500px] mb-9 text-white/80">Passionate and innovative thinker experienced in Frontend, Backend and Mobile Development. I also contribute my capabilities to non-profits and open source projects</p>
-              {/* btn and specials */}
-              <div className="flex flex-col xl:flex-row items-center gap-8:">
-                <Button
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">Passionate and innovative thinker experienced in Frontend, Backend and Mobile Development. I also contribute my capabilities to non-profits and open source projects</p>
+            {/* btn and specials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8:">
+              
+              <Button
                 variant="outline"
                 size="lg"
-                className="uppercase flex items-center gap-2  "
-                >
-                  <span>Download CV</span>
-                  <FiDownload className="text-xl" />
-                  </Button> 
-                  <div className="mb-8 xl:mb-0">
-                    <Social containerStyles="flex gap-6 m-5" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
-                  </div>
-              </div>
-          </div>
-          {/* photo */}
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo />
+                className="uppercase flex items-center gap-2"
+                onClick={() => { downloadFile(PNG_FILE_URL); }}
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+            
+            <div className="mb-8 xl:mb-0">
+              <Social containerStyles="flex gap-6 m-5" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+            </div>
           </div>
         </div>
+        {/* photo */}
+        <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <Photo />
+        </div>
+      </div>
       </div>
       <Stats />
     </section>
   );
 };
 
-export default Home
+export default Home;
